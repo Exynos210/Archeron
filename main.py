6d59848a5f6f84a2a74f3cec3de4554b5feff04b
AvFl4028 <aviel4028@gmail.com>
2024-03-24 14:25:06 -0600

    update

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-def main():
-    print("test")
+# This example requires the 'message_content' intent.
+import discord
+from discord.ext import commands 
+import os 
 
-if __name__ == "__main__":
-    main()
+bot_token:str = os.environ.get("TOKEN")
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())  # Prefijo para los comandos
+
+@bot.event
+async def on_ready():
+    print(f"Conectado como {bot.user.name}")
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.reply("Pong!")
+
+
+@bot.event
+async def on_message(ctx):
+    if ctx.author == bot.user:
+        return
+
+    if ctx.content.startswith("$hello"):
+        await ctx.channel.send("Hello World!")
+
+
+# bot_token es una variable con env del token original
+bot.run(bot_token)
